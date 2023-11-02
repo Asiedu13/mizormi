13417e154ac6ced0beac5eeeb6285dabec819ee0
Prince Asiedu <princekofasiedu@gmail.com>
2023-11-02 15:33:04 +0000

    Drivers: Enable authorised admin access to Drivers model

--- app/api/drivers/route.js
+++ app/api/drivers/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
-import { v4 as uuidv4 } from "uuid";
+import { headers } from "next/headers";
+
 import {
   collection,
   doc,
@@ -10,34 +11,47 @@ import {
 } from "firebase/firestore";
 import { db } from "../firebase";
 
+import { v4 as uuidv4 } from "uuid";
+
+import { AUTH_ERROR_MESSAGE, isAdmin } from "../utils";
+
 const driversRef = collection(db, "drivers");
 
 export async function GET(request) {
-  let data = [];
-  const q = query(driversRef, limit(10));
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-    data.push(doc.data());
-  });
+  const userAPIKey = headers().get("authorization");
+  if (await isAdmin(userAPIKey)) {
+    let data = [];
+    const q = query(driversRef, limit(10));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      data.push(doc.data());
+    });
 
-  return NextResponse.json({ data });
+    return NextResponse.json({ data });
+  } else {
+    return NextResponse.json({ data: AUTH_ERROR_MESSAGE, status: false });
+  }
 }
 
 export async function POST(request) {
-  const driverId = uuidv4();
-  const res = await request.json();
+  const userAPIKey = headers().get("authorization");
+  if (await isAdmin(userAPIKey)) {
+    const driverId = uuidv4();
+    const res = await request.json();
 
-  const newDriver = await setDoc(doc(driversRef, driverId), {
-    id: driverId,
-    primary_bus_assigned: res.primary_bus_assigned, // TODO: USe station IDs instead
-    driver_license_number: res.driver_license_number,
-    full_name: res.full_name,
-    profile_photo_url: res.profile_photo_url,
+    const newDriver = await setDoc(doc(driversRef, driverId), {
+      id: driverId,
+      primary_bus_assigned: res.primary_bus_assigned, // TODO: USe station IDs instead
+      driver_license_number: res.driver_license_number,
+      full_name: res.full_name,
+      profile_photo_url: res.profile_photo_url,
+    });
 
-  });
-
-  return NextResponse.json({
-    data: `Driver ${driverId} created successfully`,
-    status: true,
-  });
+    return NextResponse.json({
+      data: `Driver ${driverId} created successfully`,
+      status: true,
+    });
+  } else {
+    return NextResponse.json({ data: AUTH_ERROR_MESSAGE, status: false });
+  }
 }
