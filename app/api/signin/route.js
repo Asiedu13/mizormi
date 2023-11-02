14d3073edc311f9d9c7ffab7ce1781c79b6d4470
Prince Asiedu <princekofasiedu@gmail.com>
2023-11-02 15:25:07 +0000

    feature: registration enabled

---
+++ app/api/signin/route.js
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+import { db, signInUser } from "../firebase";
+import { getDoc, doc } from "firebase/firestore";
+
+export async function POST(request) {
+  const { email, password } = await request.json();
+
+  const user = await signInUser(email, password);
+  const apiKey = user.uid;
+
+  const docRef = doc(db, "users", apiKey);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    return NextResponse.json({ data: docSnap.data(), apiKey, status: true });
+  } else {
+    return NextResponse.json({ error: `${apiKey} does not exist` });
+  }
+  // return NextResponse.json( { data: user.providerData, apiKey, status: true } );
+}
